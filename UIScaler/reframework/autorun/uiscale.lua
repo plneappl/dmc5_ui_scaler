34843c75f6f761106146db551b1a6151735e35e3
plneappl <simon@wegendt.eu>
2023-08-07 18:01:51 +0200

    filter more stuff, reorganize code a bit

--- UIScaler/reframework/autorun/uiscale.lua
+++ UIScaler/reframework/autorun/uiscale.lua
@@ -9,11 +9,19 @@ function vec4_tostring(vec)
     return vec3_tostring(vec) .. ", " .. tostring(vec.w)
 end
 
+local gui_type = sdk.typeof("via.gui.GUI")
+
 local x_scale = 1
 local y_scale = 0.5
 
-local x_offset = 100
-local y_offset = 200
+local x_scale_cutscenes = 1
+local y_scale_cutscenes = 1
+
+local x_offset = 0
+local y_offset = 0
+
+local x_offset_cutscenes = 0
+local y_offset_cutscenes = 0
 
 local disable = false
 local reset_disabled = false
@@ -29,6 +37,14 @@ if(settings ~= nil) then
     y_offset = settings.y_offset
 
     disable = settings.disable == true
+
+    local v111Settings = settings.v111Settings
+    if v111Settings ~= nil then
+        x_scale_cutscenes = v111Settings.x_scale_cutscenes
+        y_scale_cutscenes = v111Settings.y_scale_cutscenes
+        x_offset_cutscenes = v111Settings.x_offset_cutscenes
+        y_offset_cutscenes = v111Settings.y_offset_cutscenes
+    end
 end
 
 function save_settings()
@@ -37,7 +53,13 @@ function save_settings()
         y_scale = y_scale,
         x_offset = x_offset,
         y_offset = y_offset,
-        disable = disable
+        disable = disable,
+        v111Settings = {
+            x_scale_cutscenes = x_scale_cutscenes,
+            y_scale_cutscenes = y_scale_cutscenes,
+            x_offset_cutscenes = x_offset_cutscenes,
+            y_offset_cutscenes = y_offset_cutscenes
+        }
     }
     json.dump_file(json_name, all_settings)
 end
@@ -47,59 +69,121 @@ re.on_draw_ui(function()
         changed1, disable = imgui.checkbox(" disable", disable)
         changed2, x_scale = imgui.slider_float(" x-scale", x_scale, 0, 1)
         changed3, y_scale = imgui.slider_float(" y-scale", y_scale, 0, 1)
-        changed4, x_offset = imgui.slider_float(" x-offset", x_offset, 0, 4000)
-        changed5, y_offset = imgui.slider_float(" y_offset", y_offset, 0, 1000)
-        discard, reset_disabled = imgui.checkbox(" reset disabled elements (debugging)", reset_disabled)
+        changed4, x_scale_cutscenes = imgui.slider_float(" x-scale (loading screens)", x_scale_cutscenes, 0, 1)
+        changed5, y_scale_cutscenes = imgui.slider_float(" y-scale (loading screens)", y_scale_cutscenes, 0, 1)
+        changed6, x_offset = imgui.slider_float(" x-offset", x_offset, 0, 4000)
+        changed7, y_offset = imgui.slider_float(" y_offset", y_offset, 0, 1000)
+        changed8, x_offset_cutscenes = imgui.slider_float(" x-offset (loading screens)", x_offset_cutscenes, 0, 4000)
+        changed9, y_offset_cutscenes = imgui.slider_float(" y_offset (loading screens)", y_offset_cutscenes, 0, 1000)
+        _, reset_disabled = imgui.checkbox(" reset disabled elements (debugging)", reset_disabled)
 
-        if changed1 or changed2 or changed3 or changed4 or changed5 then
+        if 
+            changed1 or 
+            changed2 or 
+            changed3 or 
+            changed4 or 
+            changed5 or 
+            changed6 or 
+            changed7 or 
+            changed8 or 
+            changed9 then
             save_settings()
         end
     end
 end)
 
-function do_scale(game_object, name)
-    return 
-        not disable and
-        name ~= "SaveLoadIcon_GUImesh" and
-        -- gauntlet overlay
-        name ~= "pickupGauntletHud" and
-        name ~= "Fade_Loading" and
-        name ~= "Fade_InGame" and
-        name ~= "Fade_Menu" and
-        -- secret mission marker
-        name ~= "SecretVision" and
-        -- overlay: start secret mission
-        name ~= "MessageLabel" and
-        name ~= "ui3104" and
-        -- credits
-        name ~= "ui6000" and
-        name ~= "ScarsGUI" and
-        name ~= "ScarsRoughnessGUI" and
-        name ~= "ScarsNormalGUI" and
-        --name:find("Scars", 1, true) ~= 1 and
-        -- hp markers
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1008GUI")) == nil and
-        -- target markers
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1009GUI")) == nil and
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1010GUI")) == nil 
+local game_object_get_component = sdk.find_type_definition("via.GameObject"):get_method("getComponent(System.Type)")
+
+local should_not_scale_name = {}
+should_not_scale_name["SaveLoadIcon_GUImesh"] = true
+-- gauntlet overlay
+should_not_scale_name["pickupGauntletHud"] = true
+should_not_scale_name["Fade_Loading"] = true
+should_not_scale_name["Fade_InGame"] = true
+should_not_scale_name["Fade_Menu"] = true
+-- secret mission marker
+should_not_scale_name["SecretVision"] = true
+-- shop messages
+should_not_scale_name["ui0002"] = true
+-- loading screen
+should_not_scale_name["ui0040"] = true
+should_not_scale_name["ui0041"] = true
+should_not_scale_name["ui3107"] = true
+should_not_scale_name["ui3108_s10"] = true
+-- overlay: start secret mission
+should_not_scale_name["MessageLabel"] = true
+should_not_scale_name["ui3104"] = true
+-- overlay: secret mission instructions
+should_not_scale_name["ui7002"] = true
+-- some boss names
+should_not_scale_name["m16_200_UI"] = true
+should_not_scale_name["m17_300_UI"] = true
+-- credits
+should_not_scale_name["ui6000"] = true
+should_not_scale_name["ScarsGUI"] = true
+should_not_scale_name["ScarsRoughnessGUI"] = true
+should_not_scale_name["ScarsNormalGUI"] = true
+should_not_scale_name["Fade_Menu"] = true
+
+-- in game menu
+-- "ui3105"
+
+local should_not_scale_type = {}
+-- hp markers
+table.insert(should_not_scale_type, sdk.typeof("app.ui1008GUI"))
+-- target markers
+table.insert(should_not_scale_type, sdk.typeof("app.ui1009GUI"))
+table.insert(should_not_scale_type, sdk.typeof("app.ui1010GUI"))
+
+function has_type(list_of_types, game_object)
+    for _, type in ipairs(list_of_types) do
+        if game_object_get_component(game_object, type) ~= nil then
+            return true
+        end
+    end
+    return false
 end
+
+function do_scale(game_object, name)
+    return
+        not disable and
+        should_not_scale_name[name] ~= true and
+        not has_type(should_not_scale_type, game_object)
+end
+
+local cutscenes = {}
+-- intro
+cutscenes["StartLogo"] = true
+cutscenes["ui2201_Vergil"] = true
+-- loading screens
+cutscenes["ui3101"] = true
+cutscenes["ui3120"] = true
+cutscenes["ui8022"] = true
+
+function is_cutscene(game_object, name)
+    return cutscenes[name] == true
+end
+
+local right_aligned_types = {}
+-- gauntlet UI
+table.insert(right_aligned_types, sdk.typeof("app.ui1013GUI"))
+-- c-c-combo
+table.insert(right_aligned_types, sdk.typeof("app.ui1014GUI"))
+-- ORBS
+table.insert(right_aligned_types, sdk.typeof("app.ui1020GUI"))
 
 function draw_right_aligned(game_object, name)
-    return 
-        -- gauntlet UI
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1013GUI")) ~= nil or
-        -- ORBS
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1020GUI")) ~= nil or
-        -- c-c-combo
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1014GUI")) ~= nil
+    return has_type(right_aligned_types, game_object)
 end
 
+local bot_aligned_types = {}
+-- boss health
+table.insert(bot_aligned_types, sdk.typeof("app.ui1007GUI"))
+-- gauntlet UI
+table.insert(bot_aligned_types, sdk.typeof("app.ui1013GUI"))
+
 function draw_bot_aligned(game_object, name)
-    return
-        -- gauntlet UI
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1013GUI")) ~= nil or
-        -- boss health
-        game_object:call("getComponent(System.Type)", sdk.typeof("app.ui1007GUI")) ~= nil
+    return has_type(bot_aligned_types, game_object)
 end
 
 re.on_pre_gui_draw_element(function(element, context)
@@ -112,13 +196,23 @@ re.on_pre_gui_draw_element(function(element, context)
         if do_scale(game_object, name) then
             --logger(tostring(name))
 
-            local gui_component = game_object:call("getComponent(System.Type)", sdk.typeof("via.gui.GUI"))
+            local gui_component = game_object_get_component(game_object, gui_type)
             local view = gui_component:get_View()
 
-            view:set_Scale(Vector3f.new(x_scale, y_scale, 1))
+            local x_s = x_scale
+            local y_s = y_scale
 
-            x_off = x_offset
-            y_off = y_offset
+            local x_off = x_offset
+            local y_off = y_offset
+
+            if is_cutscene(game_object, name) then
+                x_s = x_scale_cutscenes
+                y_s = y_scale_cutscenes
+                x_off = x_offset_cutscenes
+                y_off = y_offset_cutscenes
+            end
+
+            view:set_Scale(Vector3f.new(x_s, y_s, 1))
             if draw_right_aligned(game_object, name) then
                 x_off = -x_off
             end
@@ -128,7 +222,7 @@ re.on_pre_gui_draw_element(function(element, context)
             view:set_Position(Vector3f.new(x_off, y_off, 0))
 
         elseif reset_disabled then
-            local gui_component = game_object:call("getComponent(System.Type)", sdk.typeof("via.gui.GUI"))
+            local gui_component = game_object_get_component(game_object, gui_type)
             local view = gui_component:get_View()
             view:set_Scale(Vector3f.new(1, 1, 1))
             view:set_Position(Vector3f.new(0, 0, 0))
